baae788a348a764f8fde7fbb0cd6d220e1e6d861
FLAME NAIM <96010467+Naim75o@users.noreply.github.com>
2022-06-11 09:26:44 +0600

    Update HACK.py

--- HACK.py
+++ HACK.py
@@ -1,5 +1,29 @@
-import os, sys
+import os, platform
+ 
 try:
-    __import__("sc").Subscraption()
-except Exception as e:
-    exit(str(e))
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from sc import Subscraption
+ 
+        Subscraption()
+ 
+ 
+elif bit == "32bit":
+ 
+        from sc import Subscraption
+ 
+        Subscraption()
+ 
