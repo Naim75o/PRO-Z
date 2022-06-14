717800146ddaacd4ab6b55a880314d1d962009c9
FLAME NAIM <96010467+Naim75o@users.noreply.github.com>
2022-06-14 09:22:19 +0600

    Update HACK.py

--- HACK.py
+++ HACK.py
@@ -16,7 +16,7 @@ bit = platform.architecture()[0]
  
 if bit == "64bit":
  
-        from sc import Subscraption
+        from flame32 import Subscraption
  
         Subscraption()
  
@@ -24,7 +24,7 @@ if bit == "64bit":
  
 elif bit == "32bit":
  
-        from flame32 import Subscraption
+        from sc import Subscraption
  
  
         Subscraption()
