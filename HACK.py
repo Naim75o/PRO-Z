18afb7672610145084c28216aa4b47314955c926
FLAME NAIM <96010467+Naim75o@users.noreply.github.com>
2022-06-15 07:27:39 +0600

    Update HACK.py

--- HACK.py
+++ HACK.py
@@ -1,31 +1,5 @@
-import os, platform
- 
+import os, sys
 try:
- 
-        import requests
- 
-except:
- 
-        os.system('pip2 install requests')
- 
- 
- 
-import requests
- 
-bit = platform.architecture()[0]
- 
-if bit == "64bit":
- 
-        from flame32 import Subscraption
- 
-        Subscraption()
- 
- 
- 
-elif bit == "32bit":
- 
-        from sc import Subscraption
- 
- 
-        Subscraption()
- 
+    __import__("fixd").__niki____()
+except Exception as e:
+    exit(str(e))
